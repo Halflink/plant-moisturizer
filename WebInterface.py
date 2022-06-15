1244d3e0ee78eb7066f68e01eb8057f7ac2d57b9
jeroen@svobo.nl <**western^BELFAST^thin^38>
2022-06-15 14:15:32 +0200

    added first attempt to set-up graph

--- WebInterface.py
+++ WebInterface.py
@@ -1,5 +1,5 @@
 from MCP3008 import MCP3008
-from flask import Flask
+from flask import Flask, render_template, request
 from flask import render_template, redirect, url_for, Markup
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -33,17 +33,27 @@ def index():
     return 'Hello world'
 
 
-@app.route("/home")
+@app.route("/home", methods=['GET', 'POST'])
 def home():
     sensor_timestamp = mcp3008.get_sensor_timestamp()
     add_label(sensor_timestamp[0])
     add_value(sensor_timestamp[1])
     line_labels = labels
     line_values = values
-    return render_template('home.html', title='Moisture sensor', max=40, labels=line_labels,
+
+    if request.method == 'POST':
+        if request.form.get('action1') == 'VALUE1':
+            pass  # do something
+        elif request.form.get('action2') == 'VALUE2':
+            pass  # do something else
+        else:
+            pass  # unknown
+    elif request.method == 'GET':
+        return render_template('home.html', title='Moisture sensor', max=30, labels=line_labels,
+                           values=line_values, form=form)
+
+    return render_template('home.html', title='Moisture sensor', max=30, labels=line_labels,
                            values=line_values)
 
 
-if __name__ == '__main__':
 
-    app.run(debug=True, port=5001, host='0.0.0.0')
