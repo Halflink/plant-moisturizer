cbec79d0fd8c834bdc07998f7ad71457a41021d0
jeroen@svobo.nl <**western^BELFAST^thin^38>
2022-09-13 15:44:39 +0200

    adding timestamp to readouts

--- MCP3008.py
+++ MCP3008.py
@@ -27,8 +27,7 @@ class MCP3008:
         self.sensor_data_matrix.append(sensor_data)
         if len(self.sensor_data_matrix) > self.readout_history_length:
             self.sensor_data_matrix.pop(0)  # keep only 10 readouts in the array
-        now = self.datetime.datetime.now()
-        self.sensor_data_times.append(now.strftime("%H:%M")) # self.get_time_string)
+        self.sensor_data_times.append(self.get_time_string)
         if len(self.sensor_data_times) > self.readout_history_length:
             self.sensor_data_times.pop(0) # keep only 10 readouts in the array
 
@@ -66,6 +65,7 @@ class MCP3008:
             values.append(time_stamp)
         return values
 
+    @staticmethod
     def get_time_string(self):
         now = self.datetime.datetime.now()
         return now.strftime("%H:%M")
